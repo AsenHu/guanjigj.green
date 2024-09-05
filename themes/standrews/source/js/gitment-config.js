a854e0cb06adceaa10a13f979381f3600bab8fbf
AsenHu <78863300+AsenHu@users.noreply.github.com>
2024-09-05 13:02:45 +0000

    change gitment to gitalk

--- themes/standrews/source/js/gitment-config.js
+++
@@ -1,8 +0,0 @@
-var gitment = new Gitment({
-    owner: 'AsenHu',
-    repo: 'guanjigj.green',
-    oauth: {
-        client_id: 'Ov23liaEf7biqowqFPGD',
-        client_secret: 'e08fa2162dde6e2ec054af0e2e8d4586ed617031',
-    },
-})
