35c71d8b5f89e51cfa2c956aa62b7496c5eeadcc
AsenHu <78863300+AsenHu@users.noreply.github.com>
2024-09-17 12:41:23 +0800

    change gitalk to waline (#24)

--- themes/standrews/source/js/gitalk-config.js
+++
@@ -1,9 +0,0 @@
-var gitalk = new Gitalk({
-    clientID: 'Ov23liaEf7biqowqFPGD',
-    clientSecret: 'e08fa2162dde6e2ec054af0e2e8d4586ed617031',
-    repo: 'guanjigj.green',
-    owner: 'AsenHu',
-    admin: ['AsenHu'],
-    id: location.pathname.slice(-50),      // Ensure uniqueness and length less than 50
-    distractionFreeMode: false  // Facebook-like distraction free mode
-})
